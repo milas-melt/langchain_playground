a4d8e593e3608dd864ca9aa02045ad4964403495
milas-melt <tlemcani.salim00@gmail.com>
2023-04-27 18:12:06 +0100

    tests done

---
+++ langchain_doc_quickstart/4-chat_models_test.py
@@ -0,0 +1,33 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+chat = ChatOpenAI(temperature=0)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant that translates English to French."),
+    HumanMessage(content="Translate this sentence from English to French. I love programming.")
+]
+print(chat(messages))
+# -> AIMessage(content="J'aime programmer.", additional_kwargs={})
+
+batch_messages = [
+    [
+        SystemMessage(content="You are a helpful assistant that translates English to French."),
+        HumanMessage(content="Translate this sentence from English to French. I love programming.")
+    ],
+    [
+        SystemMessage(content="You are a helpful assistant that translates English to French."),
+        HumanMessage(content="Translate this sentence from English to French. I love artificial intelligence.")
+    ],
+]
+result = chat.generate(batch_messages)
+print(result)
+# -> LLMResult(generations=[[ChatGeneration(text="J'aime programmer.", generation_info=None, message=AIMessage(content="J'aime programmer.", additional_kwargs={}))], [ChatGeneration(text="J'aime l'intelligence artificielle.", generation_info=None, message=AIMessage(content="J'aime l'intelligence artificielle.", additional_kwargs={}))]], llm_output={'token_usage': {'prompt_tokens': 71, 'completion_tokens': 18, 'total_tokens': 89}})
+
+# recover stuff
+result.llm_output['token_usage']
+# -> {'prompt_tokens': 71, 'completion_tokens': 18, 'total_tokens': 89}
